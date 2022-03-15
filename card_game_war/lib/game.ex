5fc2cade89ec8c4b87a05ab25cf868bac8eb29e1
Angel_Acuña <angel.huerta@bunsan.io>
2022-03-14 23:24:37 -0600

    juego de cartas terminado

--- card_game_war/lib/game.ex
+++ card_game_war/lib/game.ex
@@ -10,4 +10,77 @@ defmodule CardGameWar.Game do
     end
   end
 
+  def mazos do
+    cards() |> Enum.shuffle() |> Enum.chunk_every(26)
+  end
+
+  def play() do
+    [j1 | [j2]] = mazos()
+    war(j1, j2, 1)
+  end
+
+  def war([], _, _t) do
+    IO.puts("\n")
+    {:Ganador, :J2}
+  end
+  def war(_, [], _t) do
+    IO.puts("\n")
+    {:Ganador, :J1}
+  end
+  def war(j1, j2, t) do
+
+    [h1|t1] = j1
+    [h2|t2] = j2
+    IO.puts("\n     turno" <> to_string(t))
+    IO.puts("\njugador1:    mazo:" <> (j1|> length() |> to_string()))
+    x = mapp(IO.inspect(h1))
+    IO.puts("jugador2:      mazo:"<>  (j2|> length() |> to_string()))
+    y = mapp(IO.inspect(h2))
+
+    palo1 = palos(h1)
+    palo2 = palos(h2)
+
+    cond do
+      x > y ->
+        IO.puts("EL JUGADOR 1 GANA LA RONDA")
+        l = t1++[h1]++[h2]
+        war(l,t2, t+1)
+      x < y ->
+        IO.puts("EL JUGADOR 2 GANA LA RONDA")
+        l = t2++[h1]++[h2]
+        war(t1,l, t+1)
+      true ->
+
+        cond do
+          palo1 > palo2 ->
+            IO.puts("EL JUGADOR 1 GANA LA RONDA")
+            l = t1++[h1]++[h2]
+            war(l,t2, t+1)
+          true ->
+            IO.puts("EL JUGADOR 2 GANA LA RONDA")
+            l = t2++[h1]++[h2]
+            war(t1,l, t+1)
+        end
+    end
+  end
+
+  def mapp({_, valor }) do
+      x = valor
+    cond do
+      x  == :ace ->  14
+      x == :king -> 13
+      x == :queen -> 12
+      x == :jack -> 11
+      true -> valor
+    end
+  end
+
+  def palos({palo,_}) do
+    cond do
+      palo == :heart -> 4
+      palo == :diamond -> 3
+      palo == :club -> 2
+      true -> 1
+    end
+  end
 end
--- card_game_war/test/game_test.exs
+++ card_game_war/test/game_test.exs
@@ -2,35 +2,43 @@ defmodule CardGameWar.GameTest do
   use ExUnit.Case
 
   test "the highest rank wins the cards in the round" do
-    flunk("not implemented")
+    assert {:Ganador, :J2} ==
+      CardGameWar.Game.war([{:spade, 4}],[{:spade, 9}],1)
   end
 
   test "queens are higher rank than jacks" do
-    flunk("not implemented")
+    assert {:Ganador, :J1} ==
+      CardGameWar.Game.war([{:club, :queen}],[{:spade, :jack}], 1)
   end
 
   test "kings are higher rank than queens" do
-    flunk("not implemented")
+    assert {:Ganador, :J1} ==
+      CardGameWar.Game.war([{:heart, :king}],[{:heart, :queen}], 1)
   end
 
   test "aces are higher rank than kings" do
-    flunk("not implemented")
+    assert {:Ganador, :J1} ==
+      CardGameWar.Game.war([{:spade, :ace}],[{:spade, :king}], 1)
   end
 
   test "if the ranks are equal, clubs beat spades" do
-    flunk("not implemented")
+    assert {:Ganador, :J1} ==
+      CardGameWar.Game.war([{:club, :ace}],[{:spade, :ace}], 1)
   end
 
   test "if the ranks are equal, diamonds beat clubs" do
-    flunk("not implemented")
+    assert {:Ganador, :J1} ==
+      CardGameWar.Game.war([{:diamond, 2}],[{:club, 2}],1)
   end
 
   test "if the ranks are equal, hearts beat diamonds" do
-    flunk("not implemented")
+    assert {:Ganador, :J1} ==
+      CardGameWar.Game.war([{:heart, :ace}],[{:diamond, :ace}], 1)
   end
 
   test "the player loses when they run out of cards" do
-    flunk("not implemented")
+    assert {:Ganador, :J1} ==
+      CardGameWar.Game.war([{:spade, :ace}],[], 1)
   end
 
 end
