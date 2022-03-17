50d24f23ccb50b23372dafad9c02b1e387a30c61
Angel_Acuña <angel.huerta@bunsan.io>
2022-03-17 17:56:55 -0600

    En Pruebas

--- doublets/lib/solver.ex
+++ doublets/lib/solver.ex
@@ -1,13 +1,53 @@
 defmodule Doublets.Solver do
 
-  def words do
-    "./resources/words.txt"
-      |> File.stream!
-      |> Enum.to_list
-  end
+  @words "./resources/words.txt"
+    |> File.stream!()
+    |> Enum.to_list()
+    |> Enum.map(&String.trim(&1))
+  # def words do
+  #   "./resources/words.txt"
+  #   |> File.stream!()
+  #   |> Enum.to_list()
+  #   |> Enum.map(&String.trim(&1))
+  # end
 
   def doublets(word1, word2) do
-    "make me work"
+    cond do
+      String.length(word1) != String.length(word2) ->
+        []
+
+      word1 == word2 ->
+        [word1]
+
+
+      true ->
+        comparar(word1, word2, @words, [word1])
+    end
   end
 
+  def comparar(word1, word2, words, acum) do
+    wLength = String.length(word2)
+    nWords = words |> List.delete(word1|> IO.inspect())
+
+    next =
+      Enum.reduce(nWords, [], fn x, acc ->
+        a = Simetric.Levenshtein.compare(x, word1)
+
+        if a == 1 do
+          [x | acc]
+        else
+          acc
+        end
+      end) |> to_string()
+
+
+      if next == word2 do
+        [next | acum]|> Enum.reverse()
+      else
+        acum
+      # comparar(next, word2, nWords, [next|acum])
+
+
+    end
+  end
 end
